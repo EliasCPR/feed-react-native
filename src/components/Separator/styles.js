1ba5f545e0a081121229f858d801d75b2c39dadb
Elias Oliveira <elias.oliveira@adviseu.com.br>
2022-02-14 08:20:26 -0300

    application in react native

---
+++ src/components/Separator/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  divider: {
+    height: 15,
+    width: '100%',
+    backgroundColor: '#dedfe0',
+    borderStyle: 'solid',
+  },
+});
+
+export default styles;
